ba88aecaf135cd9185423bbd451052be6eef7fd6
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2023-01-05 11:04:46 +0100

    Initial commit

---
+++ src/main/java/dev/thelecrafter/advancementseeker/SeekerPlugin.java
@@ -0,0 +1,10 @@
+package dev.thelecrafter.advancementseeker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SeekerPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
